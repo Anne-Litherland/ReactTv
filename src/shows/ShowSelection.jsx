c6848dbbb068f37903282e46f52bc872ca8d1a5b
Anne Litherland <litherlandanne@gmail.com>
2025-03-13 09:11:29 -0400

    .

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,23 @@
+import { tvShows } from "./data";
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({
+  shows,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    <nav className="shows">
+      {shows.map((shows) => (
+        <a
+          className={"show" + (shows === selectedShow ? " selected" : "")}
+          key={shows.name}
+          onClick={() => setSelectedShow(shows)}
+        >
+          {shows.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
